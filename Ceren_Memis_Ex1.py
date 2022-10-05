fb21f90fa096bf7b92053b65f3328f4ba55fbd64
cerenmemis <81861803+cerenmemis@users.noreply.github.com>
2022-10-05 13:18:16 +0200

    Add files via upload

---
+++ Ceren_Memis_Ex1.py
@@ -0,0 +1,7 @@
+import numpy as np
+Matrix_1 = np.random.rand(3,3)
+print("Matrix_1: " ,"\n", Matrix_1)
+Matrix_2 = np.random.rand(3,3)
+print("Matrix_2: " ,"\n",Matrix_2)
+Result = np.dot(Matrix_1,Matrix_2)
+print("Result: ","\n",Result)
